911ffe3b221b45d7cbc1b8396737dfdc74fa4e1c
Adil Basar <descrout@gmail.com>
2022-03-23 16:19:47 +0300

    more customizations added

--- lib/src/anim_search_widget.dart
+++ lib/src/anim_search_widget.dart
@@ -12,22 +12,27 @@ class AnimSearchBar extends StatefulWidget {
   ///  closeSearchOnSuffixTap - bool , isRequired : No
   ///  suffixIcon - Icon ,isRequired :  No
   ///  prefixIcon - Icon  ,isRequired : No
+  ///  restIcon - Icon  ,isRequired : No
   ///  animationDurationInMilli -  int ,isRequired : No
   ///  helpText - String ,isRequired :  No
   /// inputFormatters - TextInputFormatter, Required - No
 
   final double width;
   final TextEditingController textController;
-  final Icon? suffixIcon;
-  final Icon? prefixIcon;
+  final Icon suffixIcon;
+  final Icon prefixIcon;
+  final Icon restIcon;
   final String helpText;
   final int animationDurationInMilli;
-  final onSuffixTap;
+  final VoidCallback? onSuffixTap;
+  final VoidCallback? onPrefixTap;
+  final Function(String value)? onSubmit;
   final bool rtl;
   final bool autoFocus;
   final TextStyle? style;
   final bool closeSearchOnSuffixTap;
-  final Color? color;
+  final Color backgroundColor;
+  final Color cursorColor;
   final List<TextInputFormatter>? inputFormatters;
 
   const AnimSearchBar({
@@ -38,15 +43,23 @@ class AnimSearchBar extends StatefulWidget {
 
     /// The textController cannot be null
     required this.textController,
-    this.suffixIcon,
-    this.prefixIcon,
-    this.helpText = "Search...",
+    this.suffixIcon = const Icon(Icons.chevron_left),
+    this.prefixIcon = const Icon(Icons.clear),
+    this.restIcon = const Icon(Icons.search),
+    this.helpText = "Search",
 
-    /// choose your custom color
-    this.color = Colors.white,
+    /// choose your custom colors
+    this.backgroundColor = Colors.white,
+    this.cursorColor = Colors.black,
 
-    /// The onSuffixTap cannot be null
-    required this.onSuffixTap,
+    /// The onSuffixTap can be null
+    this.onSuffixTap,
+
+    /// The onPrefixTap can be null
+    this.onPrefixTap,
+
+    /// The onPrefixTap can be null
+    this.onSubmit,
     this.animationDurationInMilli = 375,
 
     /// make the search bar to open from right to left
@@ -115,7 +128,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
         curve: Curves.easeOut,
         decoration: BoxDecoration(
           /// can add custom color or the color will be white
-          color: widget.color,
+          color: widget.backgroundColor,
           borderRadius: BorderRadius.circular(30.0),
           boxShadow: [
             BoxShadow(
@@ -141,7 +154,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                   padding: EdgeInsets.all(8.0),
                   decoration: BoxDecoration(
                     /// can add custom color or the color will be white
-                    color: widget.color,
+                    color: widget.backgroundColor,
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                   child: AnimatedBuilder(
@@ -149,7 +162,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                       onTap: () {
                         try {
                           ///trying to execute the onSuffixTap function
-                          widget.onSuffixTap();
+                          if (widget.onSuffixTap != null) widget.onSuffixTap!();
 
                           ///closeSearchOnSuffixTap will execute if it's true
                           if (widget.closeSearchOnSuffixTap) {
@@ -165,12 +178,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                       },
 
                       ///suffixIcon is of type Icon
-                      child: widget.suffixIcon != null
-                          ? widget.suffixIcon
-                          : Icon(
-                              Icons.close,
-                              size: 20.0,
-                            ),
+                      child: widget.suffixIcon,
                     ),
                     builder: (context, widget) {
                       ///Using Transform.rotate to rotate the suffix icon when it gets expanded
@@ -208,6 +216,9 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                     onEditingComplete: () {
                       /// on editing complete the keyboard will be closed and the search bar will be closed
                       unfocusKeyboard();
+                      if (widget.onSubmit != null) {
+                        widget.onSubmit!(widget.textController.text);
+                      }
                       setState(() {
                         toggle = 0;
                       });
@@ -217,7 +228,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                     style: widget.style != null
                         ? widget.style
                         : TextStyle(color: Colors.black),
-                    cursorColor: Colors.black,
+                    cursorColor: widget.cursorColor,
                     decoration: InputDecoration(
                       contentPadding: const EdgeInsets.only(bottom: 5),
                       isDense: true,
@@ -242,7 +253,7 @@ class _AnimSearchBarState extends State<AnimSearchBar>
             ///Using material widget here to get the ripple effect on the prefix icon
             Material(
               /// can add custom color or the color will be white
-              color: widget.color,
+              color: widget.backgroundColor,
               borderRadius: BorderRadius.circular(30.0),
               child: IconButton(
                 splashRadius: 19.0,
@@ -250,15 +261,9 @@ class _AnimSearchBarState extends State<AnimSearchBar>
                 ///if toggle is 1, which means it's open. so show the back icon, which will close it.
                 ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
                 ///prefixIcon is of type Icon
-                icon: widget.prefixIcon != null
-                    ? toggle == 1
-                        ? Icon(Icons.arrow_back_ios)
-                        : widget.prefixIcon!
-                    : Icon(
-                        toggle == 1 ? Icons.arrow_back_ios : Icons.search,
-                        size: 20.0,
-                      ),
+                icon: toggle == 1 ? widget.prefixIcon : widget.restIcon,
                 onPressed: () {
+                  if (widget.onPrefixTap != null) widget.onPrefixTap!();
                   setState(
                     () {
                       ///if the search bar is closed
